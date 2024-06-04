06fcd8325d205c17fc2f1e7bb57c846fd9c6dbd7
Nonhle Miya <nonhlew.miya@gmail.com>
2024-06-04 12:56:52 +0200

    Initial commit

---
+++ src/Components/QuestionBank.js
@@ -0,0 +1,34 @@
+const qBank = [
+    {
+        id: 1,
+        question: "What is the capital of South Africa?",
+        options: ["Durban", "Gqeberha", "Pretoria", "Johannesburg"],
+        answer: "Pretoria",
+    },
+    {
+        id: 2,
+        question: "Which continent has the highest number of countries?",
+        options: ["Asia", "Europe", "Africa", "North America"],
+        answer: "Africa",
+    },
+    {
+        id: 3,
+        question: "How many colours does the South African Flag have?",
+        options: ["5", "7", "6", "4"],
+        answer: "6"
+    },
+    {
+        id: 4,
+        question: "What is the capital of Uttarakhad?",
+        options: ["Roorkee", "Haridwar", "Dehradun", "Nanital"],
+        answer: "Dehradun"
+    },
+    {
+        id: 5,
+        question: "What is capital of Uttar Pradesh?",
+        options: ["GB Nagar", "Lucknow", "Prayagraj", "Agra"],
+        answer: "Lucknow"
+    },
+]
+ 
+export default qBank;
